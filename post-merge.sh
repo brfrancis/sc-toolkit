b63fcf743c63aa0311c055e33b53197337031208
Ben Francis <brfrancis@users.noreply.github.com>
2026-04-03 17:35:29 +0100

    Added correct PA wsgi path

--- post-merge.sh
+++ post-merge.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # Copy this file to .git/hooks/post-merge on PythonAnywhere and chmod +x it
 # It touches the WSGI file to trigger an app reload after every git pull
-touch /var/www/yourusername_pythonanywhere_com_wsgi.py  # ← update with your username
+touch /var/www/brfrancis_eu_pythonanywhere_com_wsgi.py
